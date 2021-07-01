40e9ac3de2f441a8c376ba86b6e7efe9614de631
KorotkovAlex <KorotkovAlex@users.noreply.github.com>
2021-07-01 17:20:00 +0000

    deploy: 92cb3b968aa18bfa525eb40328bcdf3a7d3b6e7b

--- src/Colors.elm
+++
@@ -1,8 +0,0 @@
-module Colors exposing (..)
-
-
-blueColor : String
-blueColor = "rgb(18, 147, 216)"
-
-whiteColor : String
-whiteColor = "rgb(256, 256, 256)"
--- src/app.js
+++
@@ -1,9 +0,0 @@
-import { Elm } from "./Main.elm";
-
-var flags = {
-  api: "http://localhost:4000",
-}
-
-Elm.Main.init({
-  flags: flags,
-});
